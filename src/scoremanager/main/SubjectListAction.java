f3816c3d44cf2fe2a617a60090a0838967f0095b
a_kato <a_kato@192.168.227.104>
2024-06-28 10:27:06 +0900

    subject(仮)

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -13,26 +13,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-import bean.Subject;
+import bean.Student;
 import tool.Action;
 
-public class SubjectListAction extends Action {
+public class SuListAction extends Action {
     public void execute(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<Subject> subjectList = new ArrayList<>();
+        List<Student> student_list = new ArrayList<>();
 
         try {
             InitialContext ic = new InitialContext();
             DataSource ds = (DataSource) ic.lookup("java:/comp/env/jdbc/hcp");
             Connection con = ds.getConnection();
-            PreparedStatement st = con.prepareStatement("SELECT * FROM SUBJECT");
+            PreparedStatement st = con.prepareStatement("SELECT NO, NAME, ENT_YEAR, CLASS_NUM, IS_ATTEND FROM STUDENT");
+//            PreparedStatement st = con.prepareStatement("SELECT NO, NAME, ENT_YEAR, CLASS_NUM, CASE WHEN IS_ATTEND THEN 'O' ELSE 'X' END AS IS_ATTEND FROM STUDENT");
             ResultSet rs = st.executeQuery();
 
             while (rs.next()) {
-                Subject subject = new Subject();
-                subject.setCd(rs.getString("cd"));
-                subject.setName(rs.getString("name"));
-                subjectList.add(subject);
+                Student student = new Student();
+                student.setNo(rs.getString("no"));
+                student.setName(rs.getString("name"));
+                student.setClassNum(rs.getString("class_num"));
+                student.setEntYear(rs.getInt("ent_year"));
+                student.setAttend(rs.getBoolean("is_attend"));
+                student_list.add(student);
             }
             st.close();
             con.close();
@@ -40,8 +44,8 @@ public class SubjectListAction extends Action {
             throw new ServletException(e);
         }
 
-        request.setAttribute("subjectList", subjectList);
-        request.getRequestDispatcher("subject_list.jsp")
+        request.setAttribute("student_list", student_list);
+        request.getRequestDispatcher("pre_student_list.jsp")
         	.forward(request, response);
     }
 }
